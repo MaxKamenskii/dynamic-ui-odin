98710a2d3fee349ece9713f4e501d95dcd50b355
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-07-13 13:43:27 +0500

    Style the image container and the buttons

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ function toggleImgSource() {
         dropDownImg.src = "./assets/dropdownarrow.svg";
     }
 }
+
+
+const prevButton = document.querySelector('.prev')
+const nextButton = document.querySelector('.next')
+
+let slideIndex = 5;
+function showSlides(n){
+    let slides = document.getElementsByClassName('imgDiv')
+    // for(const slide of slides){
+    //     console.log(slide)
+    // }
+    slides[slideIndex - 1].style.display = "block"
+    // console.log(slides[n])
+}
+
+showSlides(slideIndex)
+
+function plusSlides(){
+    let increment = slideIndex + 1;
+    showSlides(increment)
+}
+function minusSlides(){
+    let increment = slideIndex - 1;
+    showSlides(increment)
+}
+
+prevButton.addEventListener('click', ()=>{
+    console.log("button is working")
+    minusSlides()
+})
+nextButton.addEventListener('click', ()=>{
+    console.log("button is working")
+    plusSlides()
+})
